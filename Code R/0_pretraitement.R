04b52e10d5f16d99115b59148ae3423f4068488d
leciet <leslie.cieters@gmail.com>
2024-12-06 14:56:43 +0100

    modif prétraitement

--- Code R/0_pretraitement.R
+++ Code R/0_pretraitement.R
@@ -1,50 +1,52 @@
 # Leslie Cieters
 # 29/11/2024
-# Test AFMH
+# Récupération des données 
 # ------------------------------------------------------------------------------
 
 
 rm(list=ls())
-# Importation des packages
+
+
+# ------------------------- Importation des packages ---------------------------
 library(FactoMineR)
 library(Factoshiny)
 library(tidyverse)
 
-# Importation des données
-omim <- read.csv('Matrices de Jules/Profils_OMIM.csv.gz')
-phecode <- read.csv('Matrices de Jules/Profils_Phecodes.csv.gz')
+#----------------------- Importation des données--------------------------------
+omim <- read.csv('Data/Profils_OMIM.csv.gz')
+phecode <- read.csv('Data/Profils_Phecodes.csv.gz')
 
 
-# Mise en forme et juxtaposition des tableaux de données
+#----------- Mise en forme et juxtaposition des tableaux de données-------------
 colnames(omim)[1] <- "Maladie"
 colnames(phecode)[1] <- "Maladie"
 
 omim$Maladie <- as.factor(omim$Maladie)
 phecode$Maladie <- as.factor(phecode$Maladie)
 
-data_full <- rbind(omim,phecode)
+data_origin_df0 <- rbind(omim,phecode)
 
-rownames(data_full) <- data_full[,1]
-data_full <- data_full[,-1]
+rownames(data_origin_df0) <- data_origin_df0[,1]
+data_origin_df0 <- data_origin_df0[,-1]
 
 
-# vérification de la variabilité par phénotype
-min(apply(data_full[c(1:6125),],MARGIN = 1,FUN = sum))
+# ----------vérification de la variabilité par phénotype------------------------
+min(apply(data_origin_df0[c(1:6125),],MARGIN = 1,FUN = sum))
 
-max(apply(data_full,MARGIN = 1,FUN = mean))
+max(apply(data_origin_df0,MARGIN = 1,FUN = mean))
 
 # # transformation en facteurs
-# data_full <- data.frame(lapply(data_full,as.factor))
+# data_origin_df0 <- data.frame(lapply(data_origin_df0,as.factor))
 
 
 
-save(data_full,omim,phecode,new_full,file='data_clean.RData')
+save(data_origin_df0,omim,phecode,new_full,file='data_clean0.RData')
 
 # checker pour la variabilité 
 # envoyer le jeu de données propre
 # faire tourner 
 
-# res.AC <- CA(data_full[,-1],row.sup = c(6126:7090))
+# res.AC <- CA(data_origin_df0[,-1],row.sup = c(6126:7090))
 class(toto)
 
 # Graphique standard
